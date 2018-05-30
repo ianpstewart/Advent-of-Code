21c2bdfbde5391534947a4223bcfec42c5cd0ac0
Ian Stewart <ian.stewart0406@gmail.com>
2018-05-30 11:49:35 -0400

    added question and renamed file

--- dayOne/dayOneP1.py
+++ dayOne/dayOneP1.py
@@ -1,19 +1,21 @@
 #! /usr/bin/env python3
 
-inputNum = input("whats the number \n")
+print("The capatcha is: ", end = '')
+inputNum = input()
 
 capatcha = []
 for num in inputNum:
     capatcha.append(num)
-print(capatcha)
+
 
 capatcha = [int(i) for i in capatcha]
-print(capatcha)
+
 
 outputSum = 0 
 for a in range(0, len(capatcha) - 1):
      if(capatcha[a] == capatcha[a + 1]):
         outputSum = outputSum + capatcha[a]
+
 if capatcha[0] == capatcha[-1]:
     outputSum = outputSum + capatcha[0]
 
