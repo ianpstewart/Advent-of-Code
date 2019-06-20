3976636b4d0b579a14ad44f18c9cdfa40e1fdb64
Ian Stewart <ian.stewart0406@gmail.com>
2019-06-19 21:39:11 -0400

    class was a dumb idea

--- 2018/03/squaresTwo.py
+++
@@ -1,8 +0,0 @@
-class squaresTwo:
-    def __init__(self, offset, size):
-        self.offset = offset
-        self.size = size
-
-    def coords(self):
-        print(f"offset is {self.offset}")
-        
