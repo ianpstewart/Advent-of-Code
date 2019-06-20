dd778e164e5deb039578739aa39e991a41c3d6e1
Ian Stewart <ian.stewart0406@gmail.com>
2019-06-19 22:11:03 -0400

    coordinates work

--- 2018/03/three.py
+++ 2018/03/three.py
@@ -1,7 +1,6 @@
 #! /usr/bin/env python3
 
 import sys
-import squaresTwo
 
 #input.txt needs to be first argument
 in_file = sys.argv[1]
@@ -10,5 +9,18 @@ in_file = sys.argv[1]
 with open(in_file, 'r') as puzzle:
     lines = puzzle.readlines()
 
-square = squaresTwo.squaresTwo(43, 43)
-square.coords()
+#1000x1000 array of zeros
+sheet = [[0 for i in range(0,1000)] for j in range(0,1000)]
+
+xRange = []
+yRange = []
+for line in lines:
+    x = [0, 0]
+    y = [0, 0]
+    items = line.split()
+    x[0] = items[2].split(",")[0]
+    y[0] = items[2].split(",")[0]
+    x[1] = x[0] + items[3].split("x")[0]
+    y[1] = y[0] + items[3].split("x")[0]
+
+    
